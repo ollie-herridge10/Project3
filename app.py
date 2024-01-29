77fa88bd867803b62a5fb895668940253d34b1af
Ollie Herridge <515238@waes.ac.uk>
2024-01-29 13:40:56 +0000

    creating SQL Alchemy database

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('base.html')
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
