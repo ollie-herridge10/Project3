338b934c036feb8e12c9c8cf67b169850882da1a
Ollie Herridge <515238@waes.ac.uk>
2024-01-29 15:22:14 +0000

    added some jinja templating

--- app.py
+++ app.py
@@ -7,11 +7,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    # show all todos
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
 
 if __name__ == "__main__":
     db.create_all()
